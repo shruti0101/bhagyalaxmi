ccdfbec89b0796f9562adeb0ae8d4d0402690a0a
shruti0101 <148574158+shruti0101@users.noreply.github.com>
2026-01-25 13:08:00 +0530

    Update WhatsApp l

--- components/Whatsapp.jsx
+++ components/Whatsapp.jsx
@@ -6,10 +6,10 @@ import { FaWhatsapp } from "react-icons/fa";
 const Whatsapp = () => {
   return (
     <a
-      href="https://wa.me/+918826544443"
+      href="https://wa.me/+919560156328"
       target="_blank"
       rel="noopener noreferrer"
-      aria-label="Chat on WhatsApp with Shree Shakti Infratech"
+      aria-label="Chat on WhatsApp "
       className="block md:hidden fixed bottom-5 right-1 z-50 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition"
     >
       <FaWhatsapp size={28} />
